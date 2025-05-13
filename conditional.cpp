965f62c5117220493f9744f6b54d0d95d932c3db
Mannat Pal <146375401+Mp771@users.noreply.github.com>
2025-05-13 22:40:22 +0530

    Add files via upload

---
+++ conditional.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+using namespace std;
+
+  /*  int a;
+    cin >> a;
+
+if (a>0)
+ {
+    cout<< "+ve" << endl;
+}
+
+else{
+    cout<< "-ve"<< endl;
+ }
+
+ int a,b ;
+ 
+ cout<< "value of A"<< endl;
+ cin>>a;
+
+ cout << "value of B" << endl;
+ cin>>a;
+
+ if (a>b)
+ {
+   cout<< "a greater" << endl;
+}
+
+if (b>a)
+{ 
+ cout<< "b is greater"<< endl;
+} 
+
+};*/
+
+/*int a ;
+cout << "enter the value of a " << endl;
+cin >> a;
+
+if(a>0) {
+   cout<< "a is +ve" ;
+   }
+
+else{
+   
+   if (a<0){
+      cout<< "a is -ve";
+      }
+   else{
+   cout <<  "a is 0";
+}
+}
+int a ;
+cout << "enter the value of a " << endl;
+cin >> a;
+
+if(a>0) {
+   cout<< "a is +ve" ;
+   }
+
+else if(a<0){
+      cout<< "a is -ve";
+      }
+
+else {
+   cout <<  "a is 0";
+}
+int a = 9 ;
+if (a==9)
+{
+   cout<< "niney"<<endl;
+}
+
+if (a > 0){
+   cout << "+ve";
+   }
+else 
+{
+   cout <<"-ve";
+}
+int a=2;
+int b = a+1;
+
+if ((a=3)==b){
+   cout << a;
+}
+else {
+   cout << a+1;
+}
+int a = 24;
+if (a>20){
+   cout<< "lovely";
+}
+else if (a==24)
+{
+   cout<< "love";
+}
+else{
+   cout << "Babbar";
+}
+cout << a; */
+
+/*char ch;
+cout << "Enter a Character"<< endl;
+cin>> ch;
+
+if (ch>='a' && ch <='z'){
+   cout<< "Lowercase";
+   }
+else if (ch>='A' && ch <='Z'){
+   cout << "Uppercase";
+}
+else{
+   cout << "numeric";
+} 
+   
+
+int n ;
+cout << "value n";
+cin>> n;
+int i=1;
+
+while (i<=n)
+   {
+   cout << i << " ";
+   i = i+1; 
+   } 
+   
+int n;
+cout<< "give: ";
+cin>> n;
+
+int sum = 0;
+int i = 2;
+
+while (i<=n)
+{
+   sum= sum + i;
+   i= i+2 ;
+   cout<< sum<< " ";
+   }
+cout<<"Sum of even is : "<<sum<< endl;
+
+float f ;
+cout<< "Temperature in farenheit: ";
+cin>> f;
+
+float C = (f-32)*5/9 ;
+
+cout<< "F to C: "<< C;
+
+   int n ;
+   cout<< "Check: ";
+   cin>>n;
+
+   int i =2;
+   
+   while (i<n)
+   {
+      if (n%i == 0)
+      {
+         cout<<"not prime"<< endl;
+      }
+      else{
+         cout<<"Prime"<< endl;
+      }
+   i=i+1;
+   }
+   */
+  int main(){
+   /*int m ;
+   cin>> m;
+
+   int i = 1;
+
+   while (i<=m)
+   {
+      int j = 1;
+
+      while (j<=m)
+      {
+         cout<<"*";
+         j=j+1;
+      }
+      cout << endl;
+      i=i+1;
+      
+   }*/
+   
+   int m ;
+   cin>> m;
+
+   int i = 1;
+
+   while (i<=m)
+   {
+      int j = 1;
+
+      while (j<=m)
+      {
+         cout<< i;
+         j=j+1;
+      }
+      cout << endl;
+      i=i+1;
+      
+   }
+}; 
