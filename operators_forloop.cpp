6b28051e5d7b1fd7c2f009f9a1bc074648a0abde
Mannat Pal <146375401+Mp771@users.noreply.github.com>
2025-05-19 23:53:34 +0530

    Update operators_forloop.cpp

--- operators_forloop.cpp
+++ operators_forloop.cpp
@@ -62,6 +62,18 @@ int main(){
 
 /*#For Loop*/
 
+int main(){
+    int n;
+    cou<<"Enter value of n"<< endl;
+    cin>>n;
+
+    cout<<"Print count from 1 to n "<< endl;
+    for(int i = 0;i<=n,i++){
+        cout<< i << endl;        
+    }
+    
+}
+
 
 
  
