96a6cd6c90c1814e7781fde40b2decb859c180bc
Mannat Pal <146375401+Mp771@users.noreply.github.com>
2025-05-25 23:49:47 +0530

    Update conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -2,6 +2,8 @@
 #include<cmath>
 using namespace std;
 
+
+/*decimal to binary*/
 int main(){
     int n;
     cin >> n;
@@ -20,3 +22,27 @@ int main(){
     }
     cout<<"Answer is: " << ans << endl;
 }
+
+/*Binary to decimal*/
+int main() {
+    int n;
+    cin >> n;
+
+    int i = 0;
+    int ans = 0;
+
+    while (n!=0)
+    {
+        int digit = n % 10;
+
+        if (digit==1)
+        {
+            ans = ans + (pow(2, i));
+        }
+        n= n / 10;
+        i++;
+
+    }
+    cout << "Answer is: " << ans << endl;
+} 
+
