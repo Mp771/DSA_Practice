8e40520781f5e16efebbd4dc304e385b18ec49fe
Mannat Pal <146375401+Mp771@users.noreply.github.com>
2025-07-12 00:00:13 +0530

    Update conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -45,4 +45,76 @@ int main() {
     }
     cout << "Answer is: " << ans << endl;
 } 
+/*Reverse Integer*/
+int main() {
+
+    int n;
+    cout << "Number: ";
+    cin >> n;
+    int ans = 0;
+ 
+    while(n != 0 ){
+        int digit = n % 10;
+
+        if ((ans>INT_MAX/10 || (ans< INT_MIN/10))){
+            cout << "0" << endl;
+            return 0;
+        }
+       ans = (ans * 10 ) + digit;
+
+        n = n / 10;
+    } 
+    cout << "Answer is: " << ans << endl;
+}
+
+
+/*complimentt base 10*/
+int main(){
+    int n;
+    cin >> n;
+
+    // Compute bitwise NOT of input, limited to its original bit-width
+    int m = n;
+    int mask = 0;
+
+    if (m == 0) {
+        cout << "1" << endl;
+        return 0;
+    }
+
+    while (m !=0) {
+
+        mask = (mask << 1) | 1;
+        m = m >> 1;
+    }
+
+    int ans = (~n) & mask;
+    cout << ans << endl; 
+    return 0;    
+}
+/*Power of Two */
+int main(){
+    int n;
+    cin >> n;
+
+    if (n < 0) {
+        cout << "False" << endl;
+        return 0;
+    }
+    
+    for (int i = 0; i <= n; i++)
+    {
+        int ans = pow(2, i);
+
+        if (ans == n)
+        {
+            cout << "True" << endl;
+            return 0;
+        }
+    }
+    cout << "False" << endl;
+    return 0;
+}
+
+
 
