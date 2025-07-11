c369a858ec948b7cfa58f7507a4d58990d80458b
Mannat Pal <146375401+Mp771@users.noreply.github.com>
2025-07-12 00:15:55 +0530

    Update conversion.cpp

--- conversion.cpp
+++ conversion.cpp
@@ -93,7 +93,7 @@ int main(){
     return 0;    
 }
 /*Power of Two */
-int main(){
+/*1*/int main(){
     int n;
     cin >> n;
 
@@ -115,6 +115,26 @@ int main(){
     cout << "False" << endl;
     return 0;
 }
+/*2*/int main() {
+    int n;
+    cin >> n;
+
+    int ans = 1;
+ 
+    for (int i = 0; i <= 30; i++) {
+        if (ans == n)
+        {
+           cout<< "True" << endl;
+            return 0;
+        }
+        if (ans < INT_MAX/2)
+    ans = ans * 2;
+}
+    cout << "False" << endl;
+    return false;
+}
+
+
 
 
 
